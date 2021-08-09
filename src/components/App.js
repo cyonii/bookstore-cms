278097290c611b4575f6bd7295ccc1d9c8be8378
CY Kalu <cy23@tutanota.com>
2021-08-09 17:07:08 +0000

    Move App component and render higher-order components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import BookForm from '../containers/BookForm';
+import BookList from '../containers/BookList';
+
+function App() {
+  return (
+    <>
+      <BookForm />
+      <BookList />
+    </>
+  );
+}
+
+export default App;
