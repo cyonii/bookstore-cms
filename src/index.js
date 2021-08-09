201eada80cc168cc4ed2d874c84721486e1db511
CY Kalu <cy23@tutanota.com>
2021-08-09 19:22:02 +0000

    Use sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './styles/index.css';
+import './styles/index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
