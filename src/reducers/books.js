bac7b890dafc798b5272122d27e007af0e4eb117
CY Kalu <cy23@tutanota.com>
2021-08-09 16:02:42 +0000

    Finish reducer functionality
    Replaced rough code with actual functional code.

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -8,10 +8,17 @@ const initialState = [
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case CREATE_BOOK:
-      return { ...state, ...action.payload };
-    case REMOVE_BOOK:
-      return { ...state };
+    case CREATE_BOOK: {
+      const { book } = action.payload;
+
+      return { ...state, books: state.books.concat(book) };
+    }
+    case REMOVE_BOOK: {
+      const { id } = action.payload;
+      const filteredBooks = state.books.filter((book) => book.id !== id);
+
+      return { ...state, books: filteredBooks };
+    }
     default:
       return state;
   }
